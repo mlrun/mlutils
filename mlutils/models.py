e2ea15e65002840255218c6c596837aeb6649843
yasha <yashab@iguazio.com>
2020-05-08 15:03:06 +0000

    mf

--- mlutils/models.py
+++ mlutils/models.py
@@ -271,7 +271,7 @@ def eval_class_model(
                               "recall_score": metrics.recall_score(ytest, ypred)})
 
         # precision-recall
-        #gcf_clear(plt)
+        gcf_clear(plt)
         disp = metrics.plot_precision_recall_curve(model, xtest, ytest)
         disp.ax_.set_title(
             f'precision recall: AP={metrics.average_precision_score(ytest, yprob_pos):0.2f}')
@@ -279,7 +279,7 @@ def eval_class_model(
             "precision-recall-binary", body=disp.figure_))
 
         # ROC plot
-        #gcf_clear(plt)
+        gcf_clear(plt)
         fpr, tpr, _ = metrics.roc_curve(ytest, yprob_pos)
         plt.figure(1)
         plt.plot([0, 1], [0, 1], 'k--')
